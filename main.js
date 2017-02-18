233c37938c95ef12363def7fe71414ce374d55be
yui0722 <takahaimasato222@gmail.com>
2017-02-18 15:38:31 +0900

    画面調整

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ cc.game.onStart = function(){
 
     cc.view.enableRetina(false);
     cc.view.adjustViewPort(true);
-    
+
     cc.view.setDesignResolutionSize(480, 320, cc.ResolutionPolicy.SHOW_ALL);
-    cc.view.resizeWithBrowserSize(true);
+    cc.view.resizeWithBrowserSize(false);
     cc.LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new TitleScene());
     }, this);
